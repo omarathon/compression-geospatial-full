2e524b3aea59876e531938406e0c3110c83c65c4
omarathon <omsst2@cam.ac.uk>
2024-02-17 18:46:24 +0000

    .

--- code/code/cpp/compression/external/FastPFor/build/googletest-build/googlemock/CTestTestfile.cmake
+++
@@ -1,7 +0,0 @@
-# CMake generated Testfile for 
-# Source directory: /home/omar/diss/yeet/cpp/compression/external/FastPFor/build/googletest-src/googlemock
-# Build directory: /home/omar/diss/yeet/cpp/compression/external/FastPFor/build/googletest-build/googlemock
-# 
-# This file includes the relevant testing commands required for 
-# testing this directory and lists subdirectories to be tested as well.
-subdirs("../googletest")
--- code/code/cpp/compression/external/FastPFor/build/googletest-build/googletest/CTestTestfile.cmake
+++
@@ -1,6 +0,0 @@
-# CMake generated Testfile for 
-# Source directory: /home/omar/diss/yeet/cpp/compression/external/FastPFor/build/googletest-src/googletest
-# Build directory: /home/omar/diss/yeet/cpp/compression/external/FastPFor/build/googletest-build/googletest
-# 
-# This file includes the relevant testing commands required for 
-# testing this directory and lists subdirectories to be tested as well.
