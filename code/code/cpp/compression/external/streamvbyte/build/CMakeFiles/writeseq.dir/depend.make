bfc1c487ba68f3f1d1551e1dd9376ca3c100a08d
omarathon <omsst2@cam.ac.uk>
2024-02-17 18:50:12 +0000

    .

--- code/code/cpp/compression/external/streamvbyte/build/CMakeFiles/writeseq.dir/depend.make
+++
@@ -1,2 +0,0 @@
-# Empty dependencies file for writeseq.
-# This may be replaced when dependencies are built.
--- code/code/cpp/compression/external/streamvbyte/build/CTestTestfile.cmake
+++
@@ -1,8 +0,0 @@
-# CMake generated Testfile for 
-# Source directory: /home/omar/diss/code/cpp/compression/external/streamvbyte
-# Build directory: /home/omar/diss/code/cpp/compression/external/streamvbyte/build
-# 
-# This file includes the relevant testing commands required for 
-# testing this directory and lists subdirectories to be tested as well.
-add_test(unit "/home/omar/diss/code/cpp/compression/external/streamvbyte/build/unit")
-set_tests_properties(unit PROPERTIES  _BACKTRACE_TRIPLES "/home/omar/diss/code/cpp/compression/external/streamvbyte/CMakeLists.txt;188;add_test;/home/omar/diss/code/cpp/compression/external/streamvbyte/CMakeLists.txt;0;")
