735906c331c4ab7e8b6d4592a8d3d316fd98ab12
Omar Tanner <omsst2@login-p-3.data.cluster>
2024-02-24 09:21:57 +0000

    update hpc scripts

--- code/code/cpp/compression/bench_compression.sh
+++ code/code/cpp/compression/bench_compression.sh
@@ -1,1 +1,3 @@
+source ./modules.sh
+
 ./bench_tiff_comp ../../../../tiffs/geotiffs/accessibility.tif 256 1 ',zigzag,morton' 'none' ',threshold,smoothAndShift,indexBasedClassification,valueBasedClassification,valueShift'
