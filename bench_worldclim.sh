d4f2998b41a40b62dce23d44c3529ad8ca0c22d5
Omar Tanner <pixzlert@gmail.com>
2026-05-27 20:27:19 +0100

    .

--- bench_worldclim.sh
+++ bench_worldclim.sh
@@ -2,24 +2,16 @@
 set -u
 BENCH=./build/bench_comp
 
-printf "%-55s  %8s  %8s  %8s  %8s\n" \
-    "file" "c0_cf" "c0_bpi" "c3_cf" "c3_bpi"
-printf "%-55s  %8s  %8s  %8s  %8s\n" \
-    "----" "------" "------" "------" "------"
+printf "%-55s  %10s  %10s\n" "file" "c0_cfmean" "c3_cfmean"
+printf "%-55s  %10s  %10s\n" "----" "---------" "---------"
 
 for tif in /maps/omsst2/diss/others/worldclim/*.tif; do
     out=$("$BENCH" "$tif" -b 256 -n 500 \
         --ordering default --max-nodata-pct 101 \
         --check-roundtrip --normalize 2>/dev/null)
 
-    c0=$(echo "$out" | awk -F'[,:]' '$2=="0" {print $4, $8}')
-    c3=$(echo "$out" | awk -F'[,:]' '$2=="3" {print $4, $8}')
+    c0_cf=$(echo "$out" | awk -F'[,:]' '$2=="0" {print $6}')
+    c3_cf=$(echo "$out" | awk -F'[,:]' '$2=="3" {print $6}')
 
-    c0_cf=$(echo "$c0" | awk '{print $1}')
-    c0_bpi=$(echo "$c0" | awk '{print $2}')
-    c3_cf=$(echo "$c3" | awk '{print $1}')
-    c3_bpi=$(echo "$c3" | awk '{print $2}')
-
-    printf "%-55s  %8s  %8s  %8s  %8s\n" \
-        "$(basename "$tif")" "$c0_cf" "$c0_bpi" "$c3_cf" "$c3_bpi"
+    printf "%-55s  %10s  %10s\n" "$(basename "$tif")" "$c0_cf" "$c3_cf"
 done
